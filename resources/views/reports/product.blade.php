467eb84767e7e3824b47dc988754e126c5cd7675
DaraCOOKED <dara.somnag020@gmail.com>
2026-06-05 09:57:39 +0700

    fix: all report pages working

--- resources/views/reports/product.blade.php
+++ resources/views/reports/product.blade.php
@@ -94,7 +94,6 @@
                           </thead>
                           <tbody class="table-border-bottom-0">
                               @forelse ($products as $product)
-                              dd($products as $product);
                                   <tr>
                                       <td><strong>{{ $product->id }}</strong></td>
                                       <td><strong>{{ $product->series_name }}</strong></td>
